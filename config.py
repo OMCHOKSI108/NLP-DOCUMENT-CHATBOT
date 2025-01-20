1b901879b0297556c46420940dcde00e074e79aa
OM CHOKSI <134709654+OMCHOKSI108@users.noreply.github.com>
2025-01-20 21:52:24 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+# config.py
+
+# Ngrok and Cohere API keys
+config = {
+    "COHERE_API_KEY": "nY1dPyVAVgTRJpa4ynIeQsy2kq1ywRAsxqnFo8qL",  # Replace with your valid Cohere API key
+    "NGROK_AUTH_TOKEN": "2V7BIOUvIzyIJCtxMilB5jiXabK_2Qvv4tq3HqeHcZgkdZygV"  # Replace with your valid Ngrok auth token
+}
